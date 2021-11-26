5b9d5284928588a6afb857e4a6fe698884b5fe44
Billal War <83715766+BilalAshiq@users.noreply.github.com>
2021-11-26 10:01:06 -0800

    Add files via upload

---
+++ stuff/js/date.js
@@ -0,0 +1,9 @@
+
+	let now = new Date();
+	let day = now.getDay();
+	let month = now.getMonth()+1;
+	let year = now.getFullYear();
+
+	let today = day+ "/"  +month+ "/" +year;
+	let header = document.querySelector('.for');
+	header.textContent = today;
