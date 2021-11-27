5b94a5645449c0ed0e9dff7a086337ab26b74d6c
s4hil <s4hilp4rr4y@gmail.com>
2021-11-27 11:18:55 +0530

    landing page changes

--- assets/js/date.js
+++ assets/js/date.js
@@ -1,8 +1,8 @@
-let now = new Date();
-let day = now.getDay();
-let month = now.getMonth()+1;
-let year = now.getFullYear();
+// let now = new Date();
+// let day = now.getDay();
+// let month = now.getMonth()+1;
+// let year = now.getFullYear();
 
-let today = day+ "/"  +month+ "/" +year;
-let header = document.querySelector('.for');
-header.textContent = today;
+// let today = day+ "/"  +month+ "/" +year;
+// let header = document.querySelector('.for');
+// header.textContent = today;
